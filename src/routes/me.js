1b952efed828b2b34e4a8b7244862599fead5af7
youngqiankun <youngqiankun@163.com>
2019-05-14 18:49:36 +0800

    update

--- src/routes/me.js
+++ src/routes/me.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/read', (req, res) => res.send('使用nodejs和express搭建的web服务器，pm2管理服务进程!'));
+router.get('/read', (req, res) => res.send('这是使用nodejs和express搭建的web服务器，pm2管理服务进程!'));
 
 module.exports = router;
